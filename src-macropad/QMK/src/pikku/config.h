b22d52ae37d07de5a411801fc6e39ed7d4ccaf75
Biddai <hiddai@post.bgu.ac.il>
2025-03-13 21:24:31 +0200

    Fixed pins not matching the diagram in main repository

--- src-macropad/QMK/src/pikku/config.h
+++ src-macropad/QMK/src/pikku/config.h
@@ -1,5 +1,5 @@
-#define I2C1_SCL_PIN GP3
-#define I2C1_SDA_PIN GP2
+#define I2C1_SCL_PIN GP5
+#define I2C1_SDA_PIN GP4
 
 #define RP2040_BOOTLOADER_DOUBLE_TAP_RESET // Activates the double-tap behavior
 #define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_TIMEOUT 500U // Timeout window in ms in which the double tap can occur.
